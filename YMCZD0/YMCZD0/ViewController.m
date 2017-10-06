f5e68ae58801634a1e90e406ffd97ff011ca9cc8
cquxutao <178052394@qq.com>
2017-10-06 15:09:07 +0800

    remove sb

--- YMCZD0/YMCZD0/ViewController.m
+++ YMCZD0/YMCZD0/ViewController.m
@@ -21,6 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+  self.view.backgroundColor = [UIColor whiteColor];
     [self setView];
 }
 
